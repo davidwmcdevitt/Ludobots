7f93e303c97e5538eb46b6a09c20cc65bf83d894
davidwmcdevitt <31931152+davidwmcdevitt@users.noreply.github.com>
2023-01-10 00:20:26 -0600

    joints

--- generate.py
+++ generate.py
@@ -1,42 +1,63 @@
 import pyrosim.pyrosim as pyrosim
 
 
-pyrosim.Start_SDF("boxes.sdf")
-
-length = 1
-width = 1
-height = 1
-
-x = 0
-y = 0
-z = 0.5
-
-for v in range(5):
-    for g in range(5):
-        for i in range(10):
-            pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-            z = z+1
-            
-            length = length * 0.9
-            height = height * 0.9
-            width = width * 0.9
-        
-        z = 0.5
-        x = x+1
-        
-        length = 1
-        width = 1
-        height = 1
+def create_world():
+    pyrosim.Start_SDF("world.sdf")
     
-    y = y + 1
+    length = 1
+    width = 1
+    height = 1
+    
+    x = -2
+    y = -2
+    z = 0.5
+    
+    pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+    
+    
+    pyrosim.End()
+    
+    
+    
+def create_robot():
+    pyrosim.Start_URDF("body.urdf")
+    
+    length = 1
+    width = 1
+    height = 1
+    
     x = 0
+    y = 0
+    z = 0.5
+    
+    pyrosim.Send_Cube(name="BackLeg", pos=[x,y,z], size=[length,width,height])
+    
+    x = 0
+    y = 0.5
+    z = 1
+    
+    pyrosim.Send_Joint(name="BackLeg_Torso", parent="BackLeg", child="Torso", type="revolute", position=[x,y,z])
+    
+    x = 0
+    y = 0.5
+    z = 0.5
+    
+    pyrosim.Send_Cube(name="Torso", pos=[x,y,z], size=[length,width,height])
+    
+    x = 0
+    y = 0.5
+    z = -0.5
+    
+    pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[x,y,z])
+    
+    x = 0
+    y = 1
+    z = 0
+    
+    pyrosim.Send_Cube(name="FrontLeg", pos=[x,y,z], size=[length,width,height])
 
-
-
-#x = 1
-#y = 1
-#z = 1.5
-#pyrosim.Send_Cube(name="Box2", pos=[x,y,z] , size=[length,width,height])
-
-
-pyrosim.End()
+    pyrosim.End()
+    
+    
+create_world()
+create_robot()
